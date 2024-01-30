3f5ebc207549c4596afb5c13898437e6e5fa1308
SuperCoder <propscode254@gmail.com>
2024-01-30 19:55:28 +0100

    NavBarTop Works

--- src/components/Global/NavBarTop.jsx
+++ src/components/Global/NavBarTop.jsx
@@ -1,62 +1,61 @@
-import { useEffect } from "react";
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useTheme } from "../../context/userContext";
 
-const NavBarTop = () => {
-  //   use theme from local storage if available or set cupcake theme
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-  );
-
-  // update state on toggle
-  const handleToggle = (e) => {
-    if (e.target.checked) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
-  // set theme state in localstorage on mount & also update localstorage on state change
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const localTheme = localStorage.getItem("theme");
-    // add custom data-theme attribute to html tag required to update theme using DaisyUI
-    document.querySelector("html").setAttribute("data-theme", localTheme);
-  }, [theme]);
+const NavBarTop = ({
+  leftSvgComponent,
+  leftLink,
+  leftText,
+  rightLink,
+  rightSvgComponent,
+}) => {
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <>
-      {" "}
-      <div>
-        <h1 className="text-left text-primary p-8 text-4xl font-bold rounded-xl mx-auto">
-          <Link to="/">TokTok</Link>
-        </h1>
-        <label className="swap swap-rotate absolute right-6 top-8">
-          <input
-            type="checkbox"
-            onChange={handleToggle}
-            checked={theme === "light" ? false : true}
-          />
+      <div className="flex justify-between items-center w-[100%]">
+        {/* Left Side Components */}
+        <div className="flex items-center">
+          <Link to={leftLink}>{leftSvgComponent}</Link>
+          <h3 className="text-left pl-4 text-2xl font-bold rounded-xl mx-auto">
+            {leftText}
+          </h3>
+        </div>
+        {/* Right Side Components */}
+        <div className="flex  items-center gap-2 ">
+          {/* rightSvgComponent -> It is only shown when it is filled in */}
+          {rightSvgComponent && <Link to={rightLink}>{rightSvgComponent}</Link>}
+          <label className="swap swap-rotate ">
+            <input
+              type="checkbox"
+              onChange={toggleTheme}
+              checked={theme === "light" ? false : true}
+            />
 
-          {/* sun icon */}
-          <svg
-            className="swap-on fill-primary w-10 h-10"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-          </svg>
+            {/* sun icon */}
 
-          {/* moon icon */}
-          <svg
-            className="swap-off fill-primary w-10 h-10"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-          >
-            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-          </svg>
-        </label>
+            <svg
+              className="swap-on fill-primary  "
+              xmlns="http://www.w3.org/2000/svg"
+              height="34"
+              viewBox="0 -960 960 960"
+              width="34"
+            >
+              <path d="M480-83.384 362.75-200H200v-162.75L83.384-480 200-597.25V-760h162.75L480-876.616 597.25-760H760v162.75L876.616-480 760-362.75V-200H597.25L480-83.384Zm0-227.385q70.231 0 119.731-49.5T649.231-480q0-70.231-49.5-119.731T480-649.231q-70.231 0-119.731 49.5T310.769-480q0 70.231 49.5 119.731T480-310.769Zm0-40q-53.846 0-91.538-37.693-37.693-37.692-37.693-91.538t37.693-91.538q37.692-37.693 91.538-37.693t91.538 37.693q37.693 37.692 37.693 91.538t-37.693 91.538Q533.846-350.769 480-350.769ZM480-140l100-100h140v-140l100-100-100-100v-140H580L480-820 380-720H240v140L140-480l100 100v140h140l100 100Zm0-340Z" />
+            </svg>
+
+            {/* moon icon */}
+
+            <svg
+              className="swap-off fill-primary  "
+              xmlns="http://www.w3.org/2000/svg"
+              height="34"
+              viewBox="0 -960 960 960"
+              width="34"
+            >
+              <path d="M482.308-160q-133.334 0-226.667-93.333Q162.307-346.667 162.307-480q0-121.539 79.231-210.77Q320.769-780 437.693-796.154q3.23 0 6.346.231 3.115.23 6.115.692-20.231 28.231-32.038 62.808-11.808 34.577-11.808 72.423 0 106.667 74.667 181.333Q555.641-404 662.308-404q38.077 0 72.538-11.808 34.462-11.808 61.923-32.039.462 3 .693 6.116.231 3.115.231 6.346-15.385 116.923-104.616 196.154T482.308-160Zm0-40q88 0 158-48.5t102-126.5q-20 5-40 8t-40 3q-123 0-209.5-86.5t-86.5-209.5q0-20 3-40t8-40q-78 32-126.5 102t-48.5 158q0 116 82 198t198 82Zm-10-270Z" />
+            </svg>
+          </label>
+        </div>
       </div>
     </>
   );
--- src/routes/NewPostUploadRoutes/NewPost.jsx
+++ src/routes/NewPostUploadRoutes/NewPost.jsx
@@ -1,7 +1,23 @@
+import NavBarTop from "../../components/Global/NavBarTop";
+import TockTockLogoSvg from "../../components/SVG/TockTockLogoSvg";
+import HearthSvg from "../../components/SVG/HearthSvg";
+// import { useNavigate } from "react-router-dom";
 const NewPost = () => {
+  // const navigate = useNavigate();
+  // const seeFavorites = () => {
+  //   navigate("/");
+  // };
+
   return (
     <>
       <h2>NewPost</h2>
+      <NavBarTop
+        leftSvgComponent={<TockTockLogoSvg />}
+        leftLink="/"
+        leftText="Tok Tok"
+        rightSvgComponent={<HearthSvg selected={false} />}
+        rightLink="/" // NICE TO HAVE -> change to "/favorites" -> list all Favorites Post
+      />
       <section>
         <article></article>
       </section>
