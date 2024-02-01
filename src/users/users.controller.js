be1341946a57fb9cd8b0cf658f5e06385f3567d7
Melina Schild <melina.schild@icloud.com>
2024-02-01 16:52:54 +0100

    other user details

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -21,9 +21,34 @@ export const getAllUsers = async (_, res, next) => {
   }
 };
 
+// api/users?id=65a6d2382ab44516cd28a0a3
 export const getUser = async (req, res, next) => {
-  const user = req.body.user;
+  const { id } = req.query;
+  console.log({ id });
+
   try {
+    const user = await User.findById(id).select({
+      _id: 1,
+      username: 1,
+      img: 1,
+      name: 1,
+      job: 1,
+      description: 1,
+      website: 1,
+      posts: 1,
+      followers: 1,
+      following: 1,
+      favorites: 1,
+      // email: 0,
+      // birthday: 0,
+      // telephone: 0,
+      // password: 0,
+      // salt: 0,
+      // role: 0,
+      // __v: 0,
+      // updatedAt: 0,
+      // createdAt: 0,
+    });
     res.json({ user });
   } catch (err) {
     next(err);
@@ -39,14 +64,6 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  try {
-    res.end();
-  } catch (err) {
-    next(err);
-  }
-};
-
-export const getOtherUser = async (req, res, next) => {
   try {
     res.end();
   } catch (err) {
