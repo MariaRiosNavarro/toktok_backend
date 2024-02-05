0f4b3571b5b824d76dd37d7787010f7c3760093e
Andre Wolf <Andre77242@hotmail.de>
2024-02-05 16:27:15 +0100

    comment and post route secured, send user in createPost

--- src/comments/comments.router.js
+++ src/comments/comments.router.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { createComment, deleteComment, getComment, getComments, updateComment } from './comments.controller.js';
+import { verifyUser } from '../utils/middleware/auth.middleware..js';
 
 export const router = new express.Router();
 
 // UPDATE
-router.put("/editcomment/:id", updateComment)
+router.put("/editcomment/:id",verifyUser, updateComment)
 // DELETE 
-router.delete("/:id", deleteComment)
+router.delete("/:id",verifyUser, deleteComment)
 // CREATE COMMENT
-router.post("/:id/commit", async (req, res, next) => {
+router.post("/:id/commit",verifyUser, async (req, res, next) => {
     try {
         await createComment(req, res, next, true); 
     } catch (err) {
@@ -16,9 +17,9 @@ router.post("/:id/commit", async (req, res, next) => {
     }
 });
 // GET ONE
-router.get("/:id", getComment)
+router.get("/:id",verifyUser, getComment)
 // GET ALL
-router.get("/", getComments)
+router.get("/",verifyUser, getComments)
 
 
 // {"_id": ObjectId("65ba1e3cf62d099c7f3c043a")}
