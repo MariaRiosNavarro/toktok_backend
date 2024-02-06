c07508287e188faea570c7e9f6252d18f06ff308
SuperCoder <propscode254@gmail.com>
2024-02-06 15:40:18 +0100

    express config

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-import { app } from './src/app.js';
-import { connect } from './src/config/storage.config.js';
+import { app } from "./src/app.js";
+import { connect } from "./src/config/storage.config.js";
+
+const FRONTEND_INDEX = new URL(
+  "./src/frontend/dist/index.html",
+  import.meta.url
+).pathname;
+
+app.get("*", (req, res) => {
+  res.sendFile(FRONTEND_INDEX);
+});
 
 app.listen(process.env.PORT, () => {
   connect();
-  console.log('express läuft auf port:', process.env.PORT);
+  console.log("express läuft auf port:", process.env.PORT);
 });
--- src/app.js
+++ src/app.js
@@ -1,29 +1,32 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
 export const app = express();
 
-import { router as usersRouter } from './users/users.router.js';
-import { router as postsRouter } from './posts/posts.router.js';
-import { router as authRouter } from './auth/auth.router.js';
-import { router as commentsRouter } from './comments/comments.router.js'
+import { router as usersRouter } from "./users/users.router.js";
+import { router as postsRouter } from "./posts/posts.router.js";
+import { router as authRouter } from "./auth/auth.router.js";
+import { router as commentsRouter } from "./comments/comments.router.js";
 
 app.use(cors({ credentials: true, origin: process.env.ALLOWED_ORIGIN }));
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cookieParser());
 
-app.use('/api/users', usersRouter);
-app.use('/api/posts', postsRouter);
-app.use('/api/auth', authRouter);
-app.use('/api/comments', commentsRouter)
+const FRONTEND_DIR = new URL("../../frontend/dist", import.meta.url).pathname;
+app.use(express.static(FRONTEND_DIR));
+
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/comments", commentsRouter);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong!';
+  const errorMessage = err.message || "Something went wrong!";
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
