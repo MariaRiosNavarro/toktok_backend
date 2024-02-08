7472a794c12361e0e03ed23a44f0af62cf3cd4a6
SuperCoder <propscode254@gmail.com>
2024-02-08 20:29:28 +0100

    fix fake gallery

--- src/frontend/src/components/NewPostUpload/PostFakeGallery.jsx
+++ src/frontend/src/components/NewPostUpload/PostFakeGallery.jsx
@@ -2,42 +2,15 @@ const PostFakeGallery = () => {
   return (
     <>
       <section className="grid grid-cols-3 gap-[4px]">
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/131-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/171-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/451-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/493-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/774-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/803-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/853-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/953-picsum.jpg"
-        ></img>
-        <img
-          className="h-[124px] rounded-lg"
-          src="../../../public/img/1026-picsum.jpg"
-        ></img>
+        <img className="h-[124px] rounded-lg" src="/img/131-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/171-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/451-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/493-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/774-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/803-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/853-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/953-picsum.jpg"></img>
+        <img className="h-[124px] rounded-lg" src="/img/1026-picsum.jpg"></img>
       </section>
     </>
   );
